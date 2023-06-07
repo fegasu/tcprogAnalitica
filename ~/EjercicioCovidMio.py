c70d73b7ab8d7c7fcaa9a1b1a3a9b1f3ab21c318
fegasu <fegasu@misena.edu.co>
2023-06-07 16:33:39 -0500

    Update EjercicioCovidMio.py

--- ~/EjercicioCovidMio.py
+++ ~/EjercicioCovidMio.py
@@ -1,128 +1,144 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun  6 13:59:07 2023
+Spyder Editor
 
-@author: Administrador
+This is a temporary script file.
 """
+drop database if exists covid19;
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+create database if not exists COVID19;
+use covid19
 
-ruta="c:/Borrar/"
-data=pd.read_csv(ruta+'COVID19-JULIO2020.csv',low_memory=False)
-print(data.shape)
+CREATE TABLE DM_PAIS( ID_PAIS INT PRIMARY KEY AUTO_INCREMENT, NOMBRE VARCHAR(30) );
 
-data['Estado']=data['Estado'].str.replace('leve','Leve',regex=False)
-DM_ESTADO=pd.DataFrame(data['Estado'])
-DM_ESTADO.drop_duplicates(inplace=True)
-DM_ESTADO['IDESTADO']=range(1, len(DM_ESTADO) + 1)
-DM_ESTADO.set_index('IDESTADO', inplace=True)
-DM_ESTADO=DM_ESTADO.fillna('NA')
-DM_ESTADO.to_csv(ruta+'DM_ESTADO.csv')
+LOAD DATA INFILE 'c:/Borrar/DM_PAIS.csv' INTO TABLE DM_PAIS
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(ID_PAIS,NOMBRE);
 
-data['Tipo']=data['Tipo'].str.replace('relacionado','Relacionado',regex=False)
-data['Tipo']=data['Tipo'].str.replace('RELACIONADO','Relacionado',regex=False)
-data['Tipo']=data['Tipo'].str.replace('En Estudio','En estudio',regex=False)
-DM_TIPO=pd.DataFrame(data['Tipo'])
-DM_TIPO.drop_duplicates(inplace=True)
-DM_TIPO['IDTIPO']= range(1, len(DM_TIPO) + 1)
-DM_TIPO.set_index('IDTIPO', inplace=True)
-DM_TIPO.to_csv(ruta+'DM_TIPO.csv')
+CREATE TABLE DM_ATENCION(
+IDATENCION INTEGER PRIMARY KEY AUTO_INCREMENT,
+NOMBRE VARCHAR(30)
+);
 
-DM_FECHA=pd.DataFrame(data['Fecha'])
-DM_FECHA.drop_duplicates(inplace=True)
-DM_FECHA['IDFECHA']=range(1,len(DM_FECHA)+1)
-DM_FECHA.set_index('IDFECHA',inplace=True)
-DM_FECHA.to_csv(ruta+'DM_FECHA.csv')
+LOAD DATA INFILE 'c:/Borrar/DM_ATENCION.csv' INTO TABLE DM_ATENCION
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(IDATENCION,NOMBRE);
 
-DM_DEPARTAMENTO=pd.DataFrame(data['Departamento o Distrito '])
-DM_DEPARTAMENTO.drop_duplicates(inplace=True)
-DM_DEPARTAMENTO['IDDPTO']=data['Código DIVIPOLA']//1000
-DM_DEPARTAMENTO.columns=DM_DEPARTAMENTO.columns.str.replace('Departamento o Distrito ','NOMBRE')
-DM_DEPARTAMENTO.set_index('IDDPTO',inplace=True)
-DM_DEPARTAMENTO.to_csv(ruta+'DM_DEPARTAMENTO.csv')
+CREATE TABLE DM_TIPO(
+IDTIPO INTEGER PRIMARY KEY AUTO_INCREMENT,
+NOMBRE VARCHAR(30)
+);
 
-DM_CIUDAD=pd.DataFrame(data['Ciudad de ubicación'])
-DM_CIUDAD.drop_duplicates(inplace=True)
-DM_CIUDAD['IDCIUDAD']=data['Código DIVIPOLA']
-DM_CIUDAD.set_index('IDCIUDAD',inplace=True)
-DM_CIUDAD.to_csv(ruta+'DM_CIUDAD.csv')
+LOAD DATA INFILE 'c:/Borrar/DM_TIPO.csv' INTO TABLE DM_TIPO
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(IDTIPO,NOMBRE);
 
-DM_ATENCION=pd.DataFrame(data['atención'])
-DM_ATENCION=DM_ATENCION.fillna('NA')
-DM_ATENCION.drop_duplicates(inplace=True)
-DM_ATENCION['IDATENCION']=range(1,len(DM_ATENCION)+1)
-DM_ATENCION.set_index('IDATENCION',inplace=True)
-DM_ATENCION.to_csv(ruta+'DM_ATENCION.csv')
+CREATE TABLE DM_ESTADO(
+IDESTADO INTEGER PRIMARY KEY AUTO_INCREMENT,
+NOMBRE VARCHAR(30)
+);
 
-DM_PAIS=pd.DataFrame(data['País de procedencia'])
-DM_PAIS=DM_PAIS.fillna('COLOMBIA')
-data['País de procedencia'].fillna('COLOMBIA',inplace=True)
-DM_PAIS.columns=DM_PAIS.columns.str.replace('País de procedencia','NOMBRE')
-DM_PAIS['NOMBRE']=DM_PAIS.NOMBRE.str.replace('ESTADOS UNIDOS DE AMÉRICA','ESTADOS UNIDOS',regex=False)
-DM_PAIS['NOMBRE']=DM_PAIS.NOMBRE.str.replace('ESTADOS UNIDOS DE AMERICA','ESTADOS UNIDOS',regex=False)
-DM_PAIS['NOMBRE']=DM_PAIS.NOMBRE.str.replace('ARABIA SAUDÍ','ARABIA SAUDITA',regex=False)
-DM_PAIS['NOMBRE']=DM_PAIS.NOMBRE.str.replace('MÉXICO','MEXICO',regex=False)
-DM_PAIS['NOMBRE']=DM_PAIS.NOMBRE.str.replace('PANAMÁ','PANAMA',regex=False)
-DM_PAIS['NOMBRE']=DM_PAIS.NOMBRE.str.replace('PERÚ','PERU',regex=False)
-DM_PAIS.drop_duplicates(inplace=True)
-DM_PAIS['IDPAIS']=range(1,len(DM_PAIS)+1)
-DM_PAIS.set_index('IDPAIS',inplace=True)
-DM_PAIS=DM_PAIS.sort_values('NOMBRE')
-DM_PAIS.to_csv(ruta+'DM_PAIS.csv')
+LOAD DATA INFILE 'c:/Borrar/DM_ESTADO.csv' INTO TABLE DM_ESTADO
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(IDESTADO,NOMBRE);
 
-data['Sexo']=data.Sexo.str.replace('m','M',regex=False)
-data['Sexo']=data.Sexo.str.replace('f','F',regex=False)
-DM_SEXO=pd.DataFrame(data['Sexo'])
-DM_SEXO.columns=DM_SEXO.columns.str.replace('SEXO','NOMBRE')
-DM_SEXO.drop_duplicates(inplace=True)
-DM_SEXO['IDSEXO']=range(1,len(DM_SEXO)+1)
-DM_SEXO.set_index('IDSEXO',inplace=True)
-DM_SEXO.to_csv(ruta+'DM_SEXO.csv')
+CREATE TABLE DM_FECHA(
+IDFECHA INTEGER PRIMARY KEY AUTO_INCREMENT,
+FECHA DATETIME
+);
 
-data.columns=data.columns.str.replace('Tipo','IDTIPO')
-data.columns=data.columns.str.replace('Estado','IDESTADO')
-data.columns=data.columns.str.replace('Código DIVIPOLA','IDCIUDAD')
-data.columns=data.columns.str.replace('atención','IDATENCION')
-data.columns=data.columns.str.replace('País de procedencia','IDPAIS')
-data.columns=data.columns.str.replace('Sexo','IDSEXO')
-data.columns=data.columns.str.replace('Departamento o Distrito ','IDDPTO')
+LOAD DATA INFILE 'c:/Borrar/DM_FECHA.csv' INTO TABLE DM_FECHA
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(IDFECHA,FECHA);
 
-data['IDDPTO']=data['IDCIUDAD']//1000
+CREATE TABLE DM_DEPARTAMENTO(
+IDDPTO INTEGER PRIMARY KEY,
+NOMBRE TEXT
+);
 
-data.drop('Ciudad de ubicación',axis=1,inplace=True)
+LOAD DATA INFILE 'c:/Borrar/DM_DEPARTAMENTO.csv' INTO TABLE DM_DEPARTAMENTO
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(IDDPTO,NOMBRE);
 
-data['IDTIPO']=data.IDTIPO.str.replace('Importado','1',regex=False)
-data['IDTIPO']=data.IDTIPO.str.replace('Relacionado','2',regex=False)
-data['IDESTADO']=data.IDESTADO.str.replace('Leve','1',regex=False)
-data['IDESTADO']=data.IDESTADO.str.replace('Asintomático','2',regex=False)
-data['IDATENCION']=data.IDATENCION.str.replace('Recuperado','1',regex=False)
-data['IDATENCION']=data.IDATENCION.str.replace('Fallecido','2',regex=False)
-data['IDATENCION']=data.IDATENCION.str.replace('NA','3',regex=False)
-data['IDATENCION']=data.IDATENCION.str.replace('Casa','4',regex=False)
-data['IDATENCION']=data.IDATENCION.str.replace('UCI','5',regex=False)
-data['IDATENCION']=data.IDATENCION.str.replace('Hospital','6',regex=False)
-data['IDESTADO']=data.IDESTADO.str.replace('Fallecido','3',regex=False)
-data['IDTIPO']=data.IDTIPO.str.replace('En estudio','3',regex=False)
+CREATE TABLE DM_CIUDAD(
+IDCIUDAD INTEGER PRIMARY KEY,
+NOMBRE TEXT
+);
 
-pais=pd.read_csv(ruta+'DM_PAIS.csv')
+LOAD DATA INFILE 'c:/Borrar/DM_CIUDAD.csv' INTO TABLE DM_CIUDAD
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(IDCIUDAD,NOMBRE);
 
-z1=pais['IDPAIS']
-z2=pais['NOMBRE']
+CREATE TABLE DM_SEXO(
+IDSEXO INTEGER PRIMARY KEY,
+NOMBRE TEXT
+);
 
-for idpais,nombre in zip(z1,z2):
-    data['IDPAIS']=data.IDPAIS.str.replace(nombre,str(idpais),regex=False)
+LOAD DATA INFILE 'c:/Borrar/DM_SEXO.csv' INTO TABLE DM_SEXO
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(IDSEXO,NOMBRE);
 
-# mfecha=pd.read_csv(ruta+'DM_FECHA.csv')
-# z1=mfecha['IDFECHA']
-# z2=mfecha['Fecha']
+-- DROP TABLE TH_COVID19;
+CREATE TABLE if not exists TH_COVID19(
+ID INTEGER ,
+IDFECHA DATETIME,
+IDCIUDAD INTEGER,
+IDDPTO INTEGER,
+IDATENCION INTEGER,
+EDAD INTEGER,
+IDSEXO CHAR(1),
+IDTIPO INTEGER,
+IDESTADO INTEGER,
+IDPAIS INTEGER,
+GEDAD CHAR(1)
+    
+);
 
-# for idfecha,fecha in zip(z1,z2):
-#     data['IDFECHA']=data.Fecha.str.replace(fecha,str(idfecha),regex=False)
-data['ID']=range(1,len(data)+1)    
-data.set_index("ID",inplace=True)
-data.to_csv(ruta+'TH_COVID19.csv',index=False)
+LOAD DATA INFILE 'c:/Borrar/TH_COVID19.csv' INTO TABLE TH_COVID19
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(ID,IDFECHA,IDCIUDAD,IDDPTO,IDATENCION,EDAD,IDSEXO,IDTIPO,IDESTADO,IDPAIS,GEDAD);
 
 
+# UPDATE TH_COVID19 SET IDESTADO=4 WHERE IDESTADO=0;
+# ALTER TABLE TH_COVID19 ADD CONSTRAINT ESTADOFK FOREIGN KEY(IDESTADO) REFERENCES DM_ESTADO(IDESTADO);
+# UPDATE TH_COVID19 SET IDATENCION=3 WHERE IDATENCION=0;
+# ALTER TABLE TH_COVID19 ADD CONSTRAINT ATENCIONFK  FOREIGN KEY(IDATENCION) REFERENCES DM_ATENCION(IDATENCION);
+# ALTER TABLE TH_COVID19 ADD CONSTRAINT CIUDADFK FOREIGN KEY(IDCIUDAD) REFERENCES DM_CIUDAD(IDCIUDAD);
+# ALTER TABLE TH_COVID19 ADD CONSTRAINT DEPARTAMENTOFK FOREIGN KEY(IDDPTO) REFERENCES DM_DEPARTAMENTO(IDDPTO); 
+# UPDATE TH_COVID19 SET IDTIPO=3 WHERE IDTIPO=0;
+# ALTER TABLE TH_COVID19 ADD CONSTRAINT TIPOFK FOREIGN KEY(IDTIPO) REFERENCES DM_TIPO(IDTIPO);
 
+SELECT IDPAIS,SEXO,COUNT(*) FROM TH_COVID19
+WHERE IDPAIS=3
+GROUP BY  IDPAIS,SEXO WITH ROLLUP;
+
+SELECT IDSEXO,COUNT(*) FROM TH_COVID19
+GROUP BY  IDSEXO;
+
+SELECT IDSEXO,IDCIUDAD,COUNT(*) FROM TH_COVID19
+WHERE IDSEXO='F' AND IDCIUDAD=11001
+GROUP BY  IDSEXO,IDCIUDAD;
+
+CREATE TABLE CUBO_COVID19 AS
+SELECT IDFECHA,IDCIUDAD,IDDPTO,IDATENCION,EDAD,IDSEXO,IDTIPO,IDESTADO,IDPAIS,GEDAD,COUNT(*) FROM TH_COVID19
+WHERE IDSEXO='F' AND IDCIUDAD=11001
+GROUP BY  IDFECHA,IDCIUDAD,IDDPTO,IDATENCION,EDAD,IDSEXO,IDTIPO,IDESTADO,IDPAIS,GEDAD
+WITH ROLLUP;
